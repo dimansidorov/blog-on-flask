2fe5ecac237eb2b9dec5b3495b40c457a0281c3b
dimansidorov <dimkasidorow@gmail.com>
2023-03-28 19:44:19 +0200

    updated configs

--- blog/configs.py
+++ blog/configs.py
@@ -8,7 +8,7 @@ load_dotenv()
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://blog_database_7mag_user:xG4oRTjPO1T2DbRWvH4weEOhTrfJTOjz@dpg-cghh73fdvk4ml9ulrogg-a.frankfurt-postgres.render.com/blog_database_7mag'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '3a+*q6&rjy0#6^57w#6j&+j0lol_$nh*=4$^d*(@2m%r@lnis%'
     WTF_CSRF_ENABLED = True
